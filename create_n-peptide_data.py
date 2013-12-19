48b64a1f97ce46d1bd1a3d23f843a049b7a4f9e9
vaishaks <vaishak93@gmail.com>
2013-12-19 23:54:17 +0530

    N-Peptide sequences are created from positive data

--- create_n-peptide_data.py
+++ create_n-peptide_data.py
@@ -3,10 +3,11 @@ import getPositiveData as g
 def create_npeptide_data(n):
     g.start(n)
     f = open("data/positive_data.txt")
-    npep = open("data/"+str(n)+"peptides.txt", "w+")
+    npep = open("data/"+str(n)+"peptides.txt", "w")
     for line in f:
         i = 0
         while i+6 < len(line):    
-            npep.write(line[i:i+6])
+            npep.write(line[i:i+6]+"\n")
+            i += 6
 
 create_npeptide_data(6)
