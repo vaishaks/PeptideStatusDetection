e2bccb407412cfdbdfc8e62077a35b0ada442c18
Dhawal <dmjan21@gmail.com>
2013-12-20 00:08:52 +0530

    Comment Added

--- createAminoAcidIndex.py
+++ createAminoAcidIndex.py
@@ -1,6 +1,6 @@
 aaimap = {"A":0, "R":1, "N":2, "D":3, "C":4, "Q":5, "E":6, "G":7, "H":8, "I":9,
           "L":10, "K":11, "M":12, "F":13, "P":14, "S":15, "T":16, "W":17, "Y":18,
-          "V":19}
+          "V":19} #key-index pairs
 
 def create():
     aaindex = open("data/aaindex1.txt")
